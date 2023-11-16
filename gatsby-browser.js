2104a068b1f95a57cf0dfc54eb6a56810bc41e2a
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2023-11-16 14:48:30 +0700

    fix: integration API and reinit cache gitigonre

--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
-import "./src/styles/core.css"
-import "./src/styles/main-traning.css"
-import "./src/styles/main-country.css"
